c3009b20c93d033b6c26c59fdbbaa1cc786c1e1e
arman <am@paybox.money>
2018-12-12 11:18:14 +0600

    recurring fix

--- pbsdkmvisa/src/main/java/money/paybox/pbsdkmvisa/MVisaHelper.java
+++ pbsdkmvisa/src/main/java/money/paybox/pbsdkmvisa/MVisaHelper.java
@@ -6,10 +6,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import money.paybox.payboxsdk.Model.Error;
 import money.paybox.pbsdkmvisa.interfaces.MVisaScanListener;
 import money.paybox.pbsdkmvisa.ui.CameraActivity;
@@ -40,8 +38,8 @@ public class MVisaHelper extends MVisa {
     public void initPayment(String orderId, String cardId, String description, HashMap<String, String> extraParams) {
         if(pbListenerIsAdded()) {
             String recurringProfile = getRecurringProfile(cardId);
+            pbHelper.enableRecurring(36);
             if(recurringProfile!=null){
-                pbHelper.enableRecurring(36);
                 pbHelper.makeRecurringPayment(mvisa.getAmount(), orderId, recurringProfile, description, extraParams);
             } else {
                 pbHelper.initNewPayment(orderId, userId, mvisa.getAmount(), description, extraParams);
@@ -111,7 +109,7 @@ public class MVisaHelper extends MVisa {
         removePbListener();
     }
 
-    public class Receiver extends BroadcastReceiver {
+    private class Receiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             if(intent.getAction() == CameraActivity.QR_ACTION) {
